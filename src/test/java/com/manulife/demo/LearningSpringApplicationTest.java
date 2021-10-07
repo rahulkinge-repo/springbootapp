d741ef7fd83ff727736ced24c0f6bb1bcd8bbe53
rahulkinge <rk#pad01>
2021-10-07 08:38:58 -0400

    Added Test cass

---
+++ src/test/java/com/manulife/demo/LearningSpringApplicationTest.java
@@ -0,0 +1,14 @@
+package com.manulife.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LearningSpringApplicationTest {
+
+    @Test
+    void sampleTest() {
+        System.out.println("SampleTest execution");
+    }
+}
